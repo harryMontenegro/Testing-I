ccb8726a29282b8c7b75023cd131d079cc9d12ba
harryMontenegro <harryMontenegro@gmail.com>
2022-06-13 19:13:29 -0500

    mensaje

--- Clase12-Vivo/Calculadora.js
+++
@@ -1,8 +0,0 @@
-const prompt = require("prompt-sync")({ sigint: true });
-
-let operacionSuma = (num1, num2) => {
-    return num1 + num2;
-}
-
-let rsultado = operacionSuma(3,4);
-console.log(rsultado);
